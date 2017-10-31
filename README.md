f6cc2a23731d97c4af96736ad2f3dd2e2204263a
Hazibii <tkdparallax@gmail.com>
2017-10-31 23:51:17 +0800

    For Loops exercise

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Python-Tutorials
+
+All tutorials will be published on the (HACKER PAlS)[http://hackerpals.com] website.
+
+For more information about these tutorials or reporting any errors please email hasib@hackerpals.com
+
+:octocat::computer::bowtie:
---
+++ forLoops.py
@@ -0,0 +1,23 @@
+"""
+For Loop Syntax Structure
+
+for [iterating variable] in [sequence]:
+    [do something]
+
+"""
+
+# Loop through a range function
+for i in range(6):
+   print(i)
+
+
+# Iterate throught a number of values
+# range(start, stop)
+for i in range(0,5):
+   print(i)
+
+
+# You also do this
+# range(start, stop, step)
+for i in range(0,15,3):
+   print(i)
