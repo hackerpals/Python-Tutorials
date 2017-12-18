1ec747c034881cb76c1cd6c5a84207e5212778a2
Hazibii <tkdparallax@gmail.com>
2017-12-18 16:27:55 +0800

    folder restructure, new workshops

---
+++ Kids-Pthon-Workshops/Hacker-speaker/hacker.py
@@ -0,0 +1,24 @@
+'''
+Given a message, convert it into 1337
+'''
+
+TEST_MESSAGE = "Hello World!"
+##TEST_SUBSTITUTIONS = ['e','3']
+SUBSTITUTIONS = [['a','4'],['e','3'],['1','1'],['o','0'],['t','7']]
+
+### Function Section
+def encode_message(message, substitutions):
+    for s in substitutions:
+        old = s[0]
+        new = s[1]
+        message = message.replaace(old,new)
+        print("converted text = " + message)
+    print("Leaving encode_message")
+    return message
+
+
+#### Testing Section
+message = input("Type the message to be encoded here: ")
+converted_text = encode_message(message, SUBSTITUTIONS)
+print("started with "+message)
+print("Converted to "+converted_text)
---
+++ Python-Intro-Workshops/lists.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+'''
+Python Lists
+'''
+
+list1 = ['physics', 'chemistry', 'math', 'english';
+list2 = [1, 2, 3, 4, 5 ];
+list3 = ["a", 2, "c", 1950]
+
+#Printing the list
+print(list1, list2, list3)
